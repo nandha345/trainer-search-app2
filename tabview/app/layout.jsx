7ed5ebd55a0e099d5bc0409eda949418cb6202c5
nandha345 <nandhakumars040@gmail.com>
2025-11-20 09:16:22 +0530

    22

---
+++ tabview/app/layout.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Rootlayout() {
+  return (
+    <View>
+      <Text>Rootlayout</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
