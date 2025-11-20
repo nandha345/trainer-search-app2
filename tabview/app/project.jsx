28523ef0bcb348675df7c6031a29abf1eaba32fa
nandha345 <nandhakumars040@gmail.com>
2025-11-20 09:40:18 +0530

    45

--- tabview/app/project.jsx
+++ tabview/app/project.jsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Rootlayout() {
+export default function project() {
   return (
     <View>
-      <Text>Rootlayout</Text>
+      <Text>project</Text>
     </View>
   )
 }
